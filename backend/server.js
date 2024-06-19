0b0e4658ddc64e78d6f938fd4813c73888f405c5
Koshyar-r <belfi7777@gmail.com>
2024-06-19 10:59:17 -0700

    Implementing Socket

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,8 @@ import messageRoutes from './routes/message.routes.js'
 import userRoutes from './routes/user.routes.js'
 
 import connectToDB from './db/connectToDB.js'
+import { app, server } from './socket/socket.js'
 
-const app = express()
 const PORT = process.env.PORT || 5000
 
 dotnev.config()
@@ -20,7 +20,7 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     connectToDB()
     console.log(`Running on ${PORT}!`)
 })
